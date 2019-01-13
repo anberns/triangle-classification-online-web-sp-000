6d150939a3db1775bdad643234e758cc168cd4b1
Aaron Berns <anberns@gmail.com>
2019-01-13 15:04:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -36,3 +36,14 @@ class Triangle
 end
 
 
+it 'knows that triangles with no size are illegal' do
+    expect{Triangle.new(0, 0, 0).kind}.to raise_error(Triangle::TriangleError)
+  end
+
+  it 'knows that triangles with negative sides are illegal' do
+    expect{Triangle.new(3, 4, -5).kind}.to raise_error(Triangle::TriangleError)
+  end
+
+  it 'knows that triangles violating triangle inequality are illegal' do
+    expect{Triangle.new(1, 1, 3).kind}.to raise_error(Triangle::TriangleError)
+  end
