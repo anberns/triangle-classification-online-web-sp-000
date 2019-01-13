699f21392185c87a3df2c66e41a71628c77bd8ec
Aaron Berns <anberns@gmail.com>
2019-01-13 15:05:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,9 @@ class Triangle
     sorted_sides << @s3
     sorted_sides.sort!
     
-    if sorted_sides[0] + sorted_sides[1]
+    if sorted_sides[0] + sorted_sides[1] =< sorted_sides[2] 
+      raise TriangleError
+    end 
     
     if @s1 == @s2 && @s1 == @s3 
       return :equilateral
@@ -36,14 +38,14 @@ class Triangle
 end
 
 
-it 'knows that triangles with no size are illegal' do
-    expect{Triangle.new(0, 0, 0).kind}.to raise_error(Triangle::TriangleError)
-  end
-
-  it 'knows that triangles with negative sides are illegal' do
-    expect{Triangle.new(3, 4, -5).kind}.to raise_error(Triangle::TriangleError)
-  end
-
-  it 'knows that triangles violating triangle inequality are illegal' do
+it 'knows that triangles violating triangle inequality are illegal' do
     expect{Triangle.new(1, 1, 3).kind}.to raise_error(Triangle::TriangleError)
+  end
+
+  it 'knows that triangles violating triangle inequality are illegal 2' do
+    expect{Triangle.new(2, 4, 2).kind}.to raise_error(Triangle::TriangleError)
+  end
+
+  it 'knows that triangles violating triangle inequality are illegal 3' do
+    expect{Triangle.new(7, 3, 2).kind}.to raise_error(Triangle::TriangleError)
   end
